dc2dd7589977411f7ce78749bf39e1785493e8fc
hiapples <howard.2001@yahoo.com.tw>
2025-02-12 22:03:19 +0800

    2

--- src/components/road.vue
+++ src/components/road.vue
@@ -1,21 +1,23 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-
 const activeButton = ref('one');
-const activePage = ref('one'); // 用來儲存目前顯示的頁面
+const activePage = ref('one'); // 控制目前顯示的頁面
 
-const latitude = ref(null);//緯度
-const longitude = ref(null);//經度
+const latitude = ref(null); // 緯度
+const longitude = ref(null); // 經度
 const errorMessage = ref('');
+const travelTime = ref(''); // 存儲開車時間
+const apiKey = "AIzaSyCcy_8Hi2tRpUyp8CRCQlJVoXVt9Dc3SEA"; // 替換成你的 API Key
 
-//取得用戶當前位置
+// 取得使用者當前位置
 const getUserLocation = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         latitude.value = position.coords.latitude;
         longitude.value = position.coords.longitude;
+        getDrivingTime(); // 位置獲取成功後，計算開車時間
       },
       (error) => {
         errorMessage.value = '請允許存取位置或開啟 GPS';
@@ -25,24 +27,44 @@ const getUserLocation = () => {
     errorMessage.value = '您的瀏覽器不支援定位功能';
   }
 };
-// 當元件掛載時自動取得位置
+
+// 取得開車時間
+const getDrivingTime = async () => {
+  if (!latitude.value || !longitude.value) return;
+
+  const destination = '24.8011034,121.1924434'; // 關西服務區經緯度
+  const origin = `${latitude.value},${longitude.value}`;
+  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&mode=driving`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    if (data.routes.length > 0) {
+      travelTime.value = data.routes[0].legs[0].duration.text;
+    } else {
+      travelTime.value = '無法獲取時間';
+    }
+  } catch (error) {
+    travelTime.value = '錯誤，請稍後再試';
+  }
+};
+
+// 當元件掛載時執行
 onMounted(() => {
   getUserLocation();
 });
 
-
+// 切換按鈕
 const clickOne = () => {
   activeButton.value = 'one';
-  activePage.value = 'one'; // 顯示 page1
+  activePage.value = 'one';
 };
 
 const clickThree = () => {
   activeButton.value = 'three';
-  activePage.value = 'three'; // 顯示 page2
+  activePage.value = 'three';
 };
-
-
-
 </script>
 
 <template>
@@ -65,9 +87,7 @@ const clickThree = () => {
 
   <div v-if="activePage === 'three'" class="container-fluid page2 mt-2">
     <div class="row">
-      <p v-if="errorMessage">{{ errorMessage }}</p>
-      <p v-else-if="latitude && longitude">緯度: {{ latitude }}, 經度: {{ longitude }}</p>
-      <div class="col-12 d-flex justify-content-center font mt-2"><a href="https://www.google.com.tw/maps/place/%E9%97%9C%E8%A5%BF%E6%9C%8D%E5%8B%99%E5%8D%80/@24.8011082,121.1898685,17z/data=!3m2!4b1!5s0x34683ebdbe57f375:0xd0735f997f005528!4m6!3m5!1s0x3468394000000001:0xe6a8cc38096ec213!8m2!3d24.8011034!4d121.1924434!16s%2Fg%2F155rzm1n?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D">關西服務區</a></div>
+      <div class="col-12 d-flex justify-content-center font mt-2"><a href="https://www.google.com.tw/maps/place/%E9%97%9C%E8%A5%BF%E6%9C%8D%E5%8B%99%E5%8D%80/@24.8011082,121.1898685,17z/data=!3m2!4b1!5s0x34683ebdbe57f375:0xd0735f997f005528!4m6!3m5!1s0x3468394000000001:0xe6a8cc38096ec213!8m2!3d24.8011034!4d121.1924434!16s%2Fg%2F155rzm1n?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D">關西服務區</a><span v-if="travelTime">預估開車時間：{{ travelTime }}</span></div>
       <div class="col-12 d-flex justify-content-center font mt">西湖服務區</div>
       <div class="col-12 d-flex justify-content-center font mt">清水服務區</div>
       <div class="col-12 d-flex justify-content-center font mt">南投服務區</div>
