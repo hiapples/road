44fc882e669eeb04d01b2ab3604cbe130bf529d8
hiapples <howard.2001@yahoo.com.tw>
2025-03-05 21:41:49 +0800

    3

--- src/components/road.vue
+++ src/components/road.vue
@@ -4,56 +4,6 @@ import { ref, onMounted } from 'vue';
 const activeButton = ref('one');
 const activePage = ref('one'); // 控制目前顯示的頁面
 
-const latitude = ref(null); // 緯度
-const longitude = ref(null); // 經度
-const errorMessage = ref('');
-const travelTime = ref(''); // 存儲開車時間
-const apiKey = "AIzaSyCcy_8Hi2tRpUyp8CRCQlJVoXVt9Dc3SEA"; // 替換成你的 API Key
-
-// 取得使用者當前位置
-const getUserLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        latitude.value = position.coords.latitude;
-        longitude.value = position.coords.longitude;
-        getDrivingTime(); // 位置獲取成功後，計算開車時間
-      },
-      (error) => {
-        errorMessage.value = '請允許存取位置或開啟 GPS';
-      }
-    );
-  } else {
-    errorMessage.value = '您的瀏覽器不支援定位功能';
-  }
-};
-
-// 取得開車時間
-const getDrivingTime = async () => {
-  if (!latitude.value || !longitude.value) return;
-
-  const destination = '24.8011034,121.1924434'; // 關西服務區經緯度
-  const origin = `${latitude.value},${longitude.value}`;
-  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&mode=driving`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    
-    if (data.routes.length > 0) {
-      travelTime.value = data.routes[0].legs[0].duration.text;
-    } else {
-      travelTime.value = '無法獲取時間';
-    }
-  } catch (error) {
-    travelTime.value = '錯誤，請稍後再試';
-  }
-};
-
-// 當元件掛載時執行
-onMounted(() => {
-  getUserLocation();
-});
 
 // 切換按鈕
 const clickOne = () => {
@@ -76,24 +26,76 @@ const clickThree = () => {
   </div>
   <div v-if="activePage === 'one'" class="container-fluid page1 mt-2">
     <div class="row" >
-      <div class="col-12 d-flex justify-content-center font mt-2">中壢服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">湖口服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">泰安服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">西螺服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">新營服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">仁德服務區</div>
+      <div class="col-12 d-flex justify-content-center font mt-2">
+        <a href="https://www.google.com.tw/maps/place/%E4%B8%AD%E5%A3%A2%E6%9C%8D%E5%8B%99%E5%8D%80/@25.0042939,121.2492763,17z/data=!3m1!4b1!4m6!3m5!1s0x346821be57742231:0x7bf3ab53672804a2!8m2!3d25.0042891!4d121.2518512!16s%2Fg%2F155r9l4q?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>中壢服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E6%B9%96%E5%8F%A3%E6%9C%8D%E5%8B%99%E5%8D%80+%E5%8C%97%E5%90%91/@24.8609836,121.007205,17z/data=!3m1!4b1!4m6!3m5!1s0x34683135bfa440a5:0x893d9409b7597382!8m2!3d24.8609788!4d121.0120759!16s%2Fg%2F155q2hxs?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>湖口服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E6%B3%B0%E5%AE%89%E6%9C%8D%E5%8B%99%E5%8D%80/@24.3289082,120.7142303,17z/data=!3m2!4b1!5s0x3469055489f93871:0x992dbc382c0a959b!4m6!3m5!1s0x3469055511c9ab13:0x5f23c38cc1583053!8m2!3d24.3289033!4d120.7168052!16s%2Fg%2F11c6t92jt7?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>泰安服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E8%A5%BF%E8%9E%BA%E6%9C%8D%E5%8B%99%E5%8D%80+%E5%8C%97%E4%B8%8A/@23.7886624,120.4758195,17z/data=!4m6!3m5!1s0x346eb42d95f3e181:0x7a58d76d96564f1!8m2!3d23.7885532!4d120.4794235!16s%2Fg%2F11bbrjgh3z?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>西螺服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E6%96%B0%E7%87%9F%E6%9C%8D%E5%8B%99%E5%8D%80+%E5%8C%97%E5%90%91/@23.3395705,120.3078719,17z/data=!3m1!4b1!4m6!3m5!1s0x346e85a588808e71:0xbb93357e00232e56!8m2!3d23.3395656!4d120.3127428!16s%2Fg%2F155q6lvx?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>新營服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E4%BB%81%E5%BE%B7%E6%9C%8D%E5%8B%99%E5%8D%80+%E5%8D%97%E4%B8%8B/@22.9059611,120.2602006,17z/data=!3m1!4b1!4m6!3m5!1s0x346e7368e4d37d4f:0xc6920674817516ae!8m2!3d22.9059562!4d120.2650715!16s%2Fg%2F155q85yv?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>仁德服務區</span>
+        </a>
+      </div>
     </div>
   </div>
 
   <div v-if="activePage === 'three'" class="container-fluid page2 mt-2">
     <div class="row">
-      <div class="col-12 d-flex justify-content-center font mt-2"><a href="https://www.google.com.tw/maps/place/%E9%97%9C%E8%A5%BF%E6%9C%8D%E5%8B%99%E5%8D%80/@24.8011082,121.1898685,17z/data=!3m2!4b1!5s0x34683ebdbe57f375:0xd0735f997f005528!4m6!3m5!1s0x3468394000000001:0xe6a8cc38096ec213!8m2!3d24.8011034!4d121.1924434!16s%2Fg%2F155rzm1n?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D">關西服務區</a><span v-if="travelTime">預估開車時間：{{ travelTime }}</span></div>
-      <div class="col-12 d-flex justify-content-center font mt">西湖服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">清水服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">南投服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">古坑服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">東山服務區</div>
-      <div class="col-12 d-flex justify-content-center font mt">關廟服務區</div>
+      <div class="col-12 d-flex justify-content-center font mt-2">
+        <a href="https://www.google.com.tw/maps/place/%E9%97%9C%E8%A5%BF%E6%9C%8D%E5%8B%99%E5%8D%80/@24.8011082,121.1898685,17z/data=!3m2!4b1!5s0x34683ebdbe57f375:0xd0735f997f005528!4m6!3m5!1s0x3468394000000001:0xe6a8cc38096ec213!8m2!3d24.8011034!4d121.1924434!16s%2Fg%2F155rzm1n?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D">
+          <span>關西服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E8%A5%BF%E6%B9%96%E6%9C%8D%E5%8B%99%E5%8D%80/@24.5650371,120.7582243,17z/data=!3m1!4b1!4m6!3m5!1s0x3469a92908285b93:0x390f4ae6014a0bd0!8m2!3d24.5650322!4d120.7607992!16s%2Fg%2F155skv09?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>西湖服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E6%B8%85%E6%B0%B4%E6%9C%8D%E5%8B%99%E5%8D%80/@24.2812291,120.5987964,17z/data=!3m1!4b1!4m6!3m5!1s0x346913ecf69b99bf:0x961430d5c3397117!8m2!3d24.2812242!4d120.6013713!16s%2Fg%2F155sgwzc?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>清水服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">        
+        <a href="https://www.google.com.tw/maps/place/%E5%8D%97%E6%8A%95%E6%9C%8D%E5%8B%99%E5%8D%80/@23.8998685,120.7088229,17z/data=!3m1!4b1!4m6!3m5!1s0x346931f909c92b0d:0xce991d07c3f880a2!8m2!3d23.8998636!4d120.7113978!16s%2Fg%2F1q63cjv6n?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>南投服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E5%8F%A4%E5%9D%91%E6%9C%8D%E5%8B%99%E5%8D%80/@23.6084226,120.5419719,17z/data=!3m1!4b1!4m6!3m5!1s0x346ec75a41688cf9:0x3daade3891d1f8fe!8m2!3d23.6084177!4d120.5445468!16s%2Fg%2F155q8jtg?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>古坑服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E6%9D%B1%E5%B1%B1%E6%9C%8D%E5%8B%99%E5%8D%80/@23.286527,120.3959486,17z/data=!3m2!4b1!5s0x346e8ed236a864cb:0xf4da966f0bf7c4e5!4m6!3m5!1s0x346e0448d309050f:0x60730b3b3aae9d49!8m2!3d23.2865221!4d120.3985235!16s%2Fg%2F155r26yf?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>東山服務區</span>
+        </a>
+      </div>
+      <div class="col-12 d-flex justify-content-center font mt">
+        <a href="https://www.google.com.tw/maps/place/%E9%97%9C%E5%BB%9F%E6%9C%8D%E5%8B%99%E5%8D%80+(%E5%8D%97%E5%90%91)/@22.9315004,120.3484191,17z/data=!3m1!4b1!4m6!3m5!1s0x346e6d9359db80f3:0xc9950b5923eab1e1!8m2!3d22.9314955!4d120.350994!16s%2Fg%2F11s7r3s56d?hl=zh-TW&entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D">
+          <span>關廟服務區</span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
